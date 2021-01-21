f49e07a502f1131dc5ca256b90effa6303c36527
jfbillette <77753380+jfbillette@users.noreply.github.com>
2021-01-21 17:30:30 -0500

    Update environment.prod.ts

--- Source/DashboardTabApp/src/environments/environment.prod.ts
+++ Source/DashboardTabApp/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://uottawaqbot.azurewebsites.net/api/Request/",
+    apiBaseUrl: "https://uoqbot.azurewebsites.net/api/Request/",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
